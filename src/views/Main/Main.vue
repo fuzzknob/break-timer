3b454906cac453fa089848117281c7925d22225a
Gagan Rai <gagan_rai@outlook.com>
2020-04-17 15:11:13 +0545

    feat: Change the text of heading

--- src/views/Main/Main.vue
+++ src/views/Main/Main.vue
@@ -2,7 +2,7 @@
   <div class="flex justify-center items-center">
     <div class="flex flex-col mb-5">
       <h5 class="text-xl">
-        Next short break in...
+        {{ message }}
       </h5>
       <Timer />
       <div class="flex justify-center mt-5">
@@ -15,7 +15,7 @@
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapState, mapActions } from 'vuex'
 import Button from '@/components/Button'
 import intervalTimer from '@/utils/Timer'
 import Timer from './components/Timer'
@@ -30,6 +30,18 @@ export default {
     return {
       stopTimer: null,
     }
+  },
+  computed: {
+    ...mapState(['status', 'successionCount']),
+    message() {
+      if (this.status === 'WORK') {
+        return `Next ${this.successionCount <= 0 ? 'long break' : 'short break'} in...`
+      }
+      if (this.status === 'BREAK') {
+        return 'Break end in...'
+      }
+      return 'Break Timer'
+    },
   },
   methods: {
     ...mapActions([
