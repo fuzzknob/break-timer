3c49fc28274cd99d49b926b63e524468da13a681
Gagan Rai <gagan_rai@outlook.com>
2020-04-17 19:18:00 +0545

    feat: add notification sound

--- src/domain/Notifications.js
+++ src/domain/Notifications.js
@@ -1,6 +1,11 @@
 import { notify } from '@/libs/Notifications'
 import { convertToMinuteSeconds } from '@/utils'
 
+export function playNotificationSound() {
+  const sound = new Audio('sounds/notification.wav')
+  sound.play()
+}
+
 export function notifyBreakTime(time) {
   const { minutes, seconds } = convertToMinuteSeconds(time)
   let wordTime = ''
@@ -10,9 +15,11 @@ export function notifyBreakTime(time) {
   if (seconds) {
     wordTime += wordTime ? ` and ${seconds} seconds` : `${seconds} seconds`
   }
+  playNotificationSound()
   notify(`It's time to take break for ${wordTime}.`)
 }
 
 export function notifyResume() {
+  playNotificationSound()
   notify('Break is over. Time to work again.')
 }
