16447dcc97cd38943c1499b9655485e36ef90982
Gagan Rai <gagan_rai@outlook.com>
2020-04-17 10:13:24 +0545

    feat: add setting js for various setting

---
+++ src/domain/Settings.js
@@ -0,0 +1,15 @@
+export function getShortBreakInterval() {
+  return 600000
+}
+
+export function getShortBreakLength() {
+  return 30000
+}
+
+export function getBreakSuccession() {
+  return 6
+}
+
+export function getLongBreakLength() {
+  return 600000
+}
