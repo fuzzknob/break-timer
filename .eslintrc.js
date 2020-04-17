09af5e1471bf1c81d0d641b1242d926c2aa60d42
Gagan Rai <gagan_rai@outlook.com>
2020-04-17 09:30:28 +0545

    fix: disable prefer default export

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,6 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     semi: ['error', 'never'],
     'import/extensions': 'off',
+    'import/prefer-default-export': 'off',
   },
 };
