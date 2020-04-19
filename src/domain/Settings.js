5c39cc3a62644d398270cda550c8cded93f3da58
Gagan Rai <gagan_rai@outlook.com>
2020-04-19 18:17:05 +0545

    feat: get settings from localstorage

--- src/domain/Settings.js
+++ src/domain/Settings.js
@@ -1,15 +1,35 @@
 export function getShortBreakInterval() {
-  return 600000
+  let breakInterval = localStorage.getItem('short-break-interval')
+  if (breakInterval == null) {
+    breakInterval = 600000
+    localStorage.setItem('short-break-interval', 600000)
+  }
+  return parseInt(breakInterval, 0)
 }
 
 export function getShortBreakLength() {
-  return 30000
+  let shortBreakLength = localStorage.getItem('short-break-length')
+  if (shortBreakLength == null) {
+    shortBreakLength = 10000
+    localStorage.setItem('short-break-length', 10000)
+  }
+  return parseInt(shortBreakLength, 0)
 }
 
 export function getBreakSuccession() {
-  return 6
+  let breakSuccession = localStorage.getItem('break-succession')
+  if (breakSuccession == null) {
+    breakSuccession = 6
+    localStorage.setItem('break-succession', 6)
+  }
+  return parseInt(breakSuccession, 0)
 }
 
 export function getLongBreakLength() {
-  return 600000
+  let longBreakLength = localStorage.getItem('long-break-length')
+  if (longBreakLength == null) {
+    longBreakLength = 900000
+    localStorage.setItem('long-break-length', 900000)
+  }
+  return parseInt(longBreakLength, 0)
 }
